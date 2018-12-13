49faa749ab11ecc061552c7952770ef8db3f907f
jinlin.liu <jinlin.liu@yoho.cn>
2018-12-13 17:01:04 +0800

    book copy create

--- controllers/bookInstanceController.js
+++ controllers/bookInstanceController.js
@@ -1,4 +1,7 @@
 var BookInstance = require('../models/bookinstance');
+var Book = require('../models/book');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function(req, res, next) {
@@ -29,14 +32,54 @@ exports.bookinstance_detail = function(req, res, next) {
 };
 
 // Display BookInstance create form on GET.
-exports.bookinstance_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create GET');
+exports.bookinstance_create_get = function(req, res, next) {
+    Book.find({}, 'title').exec(function(error, books){
+        if(error){return next(error);}
+        res.render('bookinstance_form', {title:'Create Book Copies', book_list:books});
+    });
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+exports.bookinstance_create_post = [
+    // Validate fields.
+    body('book', 'Book must be specified').isLength({ min: 1 }).trim(),
+    body('imprint', 'Imprint must be specified').isLength({ min: 1 }).trim(),
+    body('due_back', 'Invalid date').optional({ checkFalsy: true }).isISO8601(),
+    
+    // Sanitize fields.
+    sanitizeBody('book').trim().escape(),
+    sanitizeBody('imprint').trim().escape(),
+    sanitizeBody('status').trim().escape(),
+    sanitizeBody('due_back').toDate(),
+
+    (req, res, next)=>{
+        const errors = validationResult(req);
+        var bookinstance = new BookInstance(
+            { book: req.body.book,
+              imprint: req.body.imprint,
+              status: req.body.status,
+              due_back: req.body.due_back
+             });
+        if(errors.isEmpty()){
+            bookinstance.save(function (err) {
+                if (err) { return next(err); }
+                    // Successful - redirect to new record.
+                    res.redirect(bookinstance.url);
+                });
+        } else {
+            Book.find({}, 'title').exec(function(error, books){
+                if(error){return next(error);}
+                res.render('bookinstance_form', {
+                    title:'Create book copies', 
+                    book_list:books,
+                    selected_book:bookinstance.book._id,
+                    errors:errors.array(),
+                    bookinstance:bookinstance
+                });
+            });
+        }
+    }
+];
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function(req, res) {
